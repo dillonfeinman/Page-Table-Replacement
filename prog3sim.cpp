824bd575321543666734f154a5af1fa06b30d45b
Evan Moeller <evancmoeller@gmail.com>
2019-03-09 11:36:58 -0500

    started work on replacement simulation

--- prog3sim.cpp
+++ prog3sim.cpp
@@ -5,6 +5,55 @@
 
 using namespace std;
 
+//Simulate OPT
+vector<float> simOPT(vector<vector<int>> softCache, vector<int> input){
+
+}
+
+//Simulate LRU
+vector<float> simLRU(vector<vector<int>> softCache, vector<int> input){
+	
+}
+
+//Simulate FIFO
+vector<float> simLRU(vector<vector<int>> softCache, vector<int> input){
+	
+}
+
+//Simulate RAND
+vector<float> simLRU(vector<vector<int>> softCache, vector<int> input){
+	
+}
+
+//Simulate CLOCK
+vector<float> simLRU(vector<vector<int>> softCache, vector<int> input){
+	
+}
+
+//Simulate the generated cache and access with each type of replacement
+//Return a vector of tuples containing graph coors for csv
+vector<float> simulator(vector<int> cache,vector<int> input)
+{
+	vector<float> retVal;
+	reVatl.push_back( (float) cache.size() ); //X Value
+
+	//Format the cache for replacement
+	vector<vector<int>> softCache;
+	vector<int> c1, c2;
+	for(int i = 0; i < cache.size(); i ++)
+	{
+		c1.push_back(cache[i]);
+		c2.push_back(0);
+	}
+
+	retVal.push_back(simOPT(softCache, input)); //Y value for opt
+	retVal.push_back(simLRU(softCache, input)); //Y value for lru
+	retVal.push_back(simFIFO(softCache, input)); //Y value for fifo
+	retVal.push_back(simRAND(softCache, input)); //Y value for rand
+	retVal.push_back(simCLOCK(softCache, input)); //Y value for clock
+	return retVal;
+}
+
 vector<int> generate(int num, int cachesize){
 	vector<int> random;
 	srand(time(0));
@@ -13,7 +62,6 @@ vector<int> generate(int num, int cachesize){
 	}
 	return random;
 }
-
 
 int main(int argc, char * argv[]){
 	if(argc != 3){
