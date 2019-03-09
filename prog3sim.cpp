6668a50286836ad3ff4772c02a38e2dda98c328d
Evan Moeller <evancmoeller@gmail.com>
2019-03-09 11:56:20 -0500

    Most of the legwork for replacement done

--- prog3sim.cpp
+++ prog3sim.cpp
@@ -6,45 +6,142 @@
 using namespace std;
 
 //Simulate OPT
-vector<float> simOPT(vector<vector<int>> softCache, vector<int> input){
+vector<float> simOPT(vector<vector<int>> softCache, vector<int> input)
+{
+	float hit = 0;
+	for(int i = 0; i < input.size())
+	{
+		bool inCache = false;
+		for(int j = 0; j < softCache[0].size(); j++) //Check cache
+		{
+			if(softCache[0][j]==input[i]) //In cache
+			{
+				inCache = true;
+				hit++;
+				break;
+			}
+		}
+		if(!inCache) //Not in cache needs replacement
+		{
 
+		}
+	}
+	return hit;
 }
 
 //Simulate LRU
-vector<float> simLRU(vector<vector<int>> softCache, vector<int> input){
-	
+vector<float> simLRU(vector<vector<int>> softCache, vector<int> input)
+{
+	float hit = 0;
+	for(int i = 0; i < input.size())
+	{
+		bool inCache = false;
+		for(int j = 0; j < softCache[0].size(); j++) //Check cache
+		{
+			if(softCache[0][j]==input[i]) //In cache
+			{
+				inCache = true;
+				hit++;
+				break;
+			}
+		}
+		if(!inCache) //Not in cache needs replacement
+		{
+
+		}
+	}
+	return hit;
 }
 
 //Simulate FIFO
-vector<float> simLRU(vector<vector<int>> softCache, vector<int> input){
-	
+vector<float> simLRU(vector<vector<int>> softCache, vector<int> input)
+{
+	float hit = 0;
+	for(int i = 0; i < input.size())
+	{
+		bool inCache = false;
+		for(int j = 0; j < softCache[0].size(); j++) //Check cache
+		{
+			if(softCache[0][j]==input[i]) //In cache
+			{
+				inCache = true;
+				hit++;
+				break;
+			}
+		}
+		if(!inCache) //Not in cache needs replacement
+		{
+
+		}
+	}
+	return hit;
 }
 
 //Simulate RAND
-vector<float> simLRU(vector<vector<int>> softCache, vector<int> input){
-	
+vector<float> simLRU(vector<vector<int>> softCache, vector<int> input)
+{
+	float hit = 0;
+	for(int i = 0; i < input.size())
+	{
+		bool inCache = false;
+		for(int j = 0; j < softCache[0].size(); j++) //Check cache
+		{
+			if(softCache[0][j]==input[i]) //In cache
+			{
+				inCache = true;
+				hit++;
+				break;
+			}
+		}
+		if(!inCache) //Not in cache needs replacement
+		{
+
+		}
+	}
+	return hit;
 }
 
 //Simulate CLOCK
-vector<float> simLRU(vector<vector<int>> softCache, vector<int> input){
-	
+vector<float> simLRU(vector<vector<int>> softCache, vector<int> input)
+{
+	float hit = 0;
+	for(int i = 0; i < input.size())
+	{
+		bool inCache = false;
+		for(int j = 0; j < softCache[0].size(); j++) //Check cache
+		{
+			if(softCache[0][j]==input[i]) //In cache
+			{
+				inCache = true;
+				hit++;
+				break;
+			}
+		}
+		if(!inCache) //Not in cache needs replacement
+		{
+
+		}
+	}
+	return hit;
 }
 
 //Simulate the generated cache and access with each type of replacement
 //Return a vector of tuples containing graph coors for csv
-vector<float> simulator(vector<int> cache,vector<int> input)
+vector<float> simulator(int cacheSize,vector<int> input)
 {
 	vector<float> retVal;
-	reVatl.push_back( (float) cache.size() ); //X Value
+	reVatl.push_back( (float) cacheSize ); //X Value
 
 	//Format the cache for replacement
 	vector<vector<int>> softCache;
 	vector<int> c1, c2;
-	for(int i = 0; i < cache.size(); i ++)
+	for(int i = 0; i < cacheSize; i ++)
 	{
-		c1.push_back(cache[i]);
+		c1.push_back(0);
 		c2.push_back(0);
 	}
+	softCache.push_back(c1);
+	softCache.push_back(c1);
 
 	retVal.push_back(simOPT(softCache, input)); //Y value for opt
 	retVal.push_back(simLRU(softCache, input)); //Y value for lru
@@ -77,22 +174,25 @@ int main(int argc, char * argv[]){
 		vector<int> access;
 		//int counter = 0;
 		for(int num = 5; num <= 100; num = num + 5){
-			access = generate(num_accesses, num);
-			vector<int> cache(num, -1);
-			for(int i = 0; i < access.size(); i++){ 	
-				for(int j = 0; j < cache.size(); j++){
-					if(cache[j] == -1){
-						cache[j] = access[i];
-					} else if(access[i] == cache[j]){
-						cache_hit++;
-						hit = 1;
-						break;
-					}
-				}
-				if(hit != 1){
-					//cache miss
-				}
-			}
+			access = generate(num_accesses, num); //Need to generate all 3 wordflows
+			//vector<int> cache(num, -1);
+			//for(int i = 0; i < access.size(); i++){ 	
+			//	for(int j = 0; j < cache.size(); j++){
+			//		if(cache[j] == -1){
+			//			cache[j] = access[i];
+			//		} else if(access[i] == cache[j]){
+			//			cache_hit++;
+			//			hit = 1;
+			//			break;
+			//		}
+			//	}
+			//	if(hit != 1){
+			//		//cache miss
+			//	}
+			//}
+
+			//Needs something to catch value and inputs
+			csv = simulator(num, access);
 		}
 	}
 }
